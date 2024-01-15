5d0b4b531a5173546461d9d2bd9ace5fbe83a122
Micah Asowata <andrewgary963@gmail.com>
2024-01-15 13:15:07 +0000

    feat: update function name for readability and add doc comments

--- ft.go
+++ ft.go
@@ -1,47 +1,13 @@
 package ft
 
-var fileExtensions = map[string]string{
-	".mp3":  "Audio",
-	".wav":  "Audio",
-	".aac":  "Audio",
-	".flac": "Audio",
-	".m4a":  "Audio",
-	".mp4":  "Video",
-	".avi":  "Video",
-	".mkv":  "Video",
-	".mov":  "Video",
-	".wmv":  "Video",
-	".jpg":  "Images",
-	".jpeg": "Images",
-	".png":  "Images",
-	".gif":  "Images",
-	".bmp":  "Images",
-	".tiff": "Images",
-	".tif":  "Images",
-	".pdf":  "Documents",
-	".docx": "Documents",
-	".xlsx": "Documents",
-	".pptx": "Documents",
-	".txt":  "Documents",
-	".mobi": "Documents",
-	".epub": "Documents",
-	".zip":  "Archives",
-	".rar":  "Archives",
-	".7z":   "Archives",
-	".tar":  "Archives",
-	".gz":   "Archives",
-	".exe":  "Executables",
-	".app":  "Executables",
-	".dmg":  "Executables",
-	".bin":  "Executables",
-	".msi":  "Executables",
-	".deb":  "Executables",
-	".csv":  "Data",
-	".json": "Data",
-	".xml":  "Data",
-}
+/*
+GetFileCatgory takes in a file extension, stating with the . e.g .png, .txt which can be gotten from
+filepath.Ext()
 
-func GetCategory(extension string) (string, error) {
+It returns a string on success or ft.ErrNotFound when the extension can not be located on the
+list of files
+*/
+func GetFileCategory(extension string) (string, error) {
 	category, found := fileExtensions[extension]
 	if !found {
 		return "", ErrNotFound
